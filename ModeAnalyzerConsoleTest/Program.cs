7d43fb849d8649ee82f26017fbb7d7af1950d49b
Imran56878 <mi056878@gmail.com>
2020-02-24 12:16:26 +0530

    [imran]add.MoodAnalzerTest

---
+++ ModeAnalyzerConsoleTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MoodAnalyzer;
+
+namespace ModeAnalyzerConsoleTest
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string");
+            string st = Console.ReadLine();
+            MoodTest  tc = new MoodTest(st);
+            try
+            {
+              Console.WriteLine(  tc.Mood ());
+            }
+            catch (MoodAnalyseException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
